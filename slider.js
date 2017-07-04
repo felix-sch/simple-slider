ad07bb26f6ecd6c3f80aaf99c5bc53d3d4007f26
ayhamk94 <ayham.kteash1995@gmail.com>
2017-07-04 16:03:38 +0200

    Working version of the slider

--- slider.js
+++ slider.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	
+  var margin =0;
+  var currentImage = 1;
+	$("#right").click(function(){
+    if (currentImage < 4) {
+      margin = margin - 1000;
+      $(".content").animate({"margin-left": margin+"px"});
+      currentImage++;
+    }
+
+  });
+  $("#left").click(function() {
+    if (currentImage > 1) {
+    margin = margin + 1000;
+    $(".content").animate({"margin-left":margin+"px"});
+    currentImage--;
+}
+  });
 });
