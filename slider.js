3c80ba80432135812b27b28e14aee3f251e8e657
ayhamk94 <ayham.kteash1995@gmail.com>
2017-07-04 16:27:31 +0200

    Working version of the slider and hidding the buttons on last image

--- slider.js
+++ slider.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
   var margin =0;
   var currentImage = 1;
-	$("#right").click(function(){
+  $("#right").click(function(){
+    $("#left").show();
     if (currentImage < 4) {
       margin = margin - 1000;
       $(".content").animate({"margin-left": margin+"px"});
       currentImage++;
+      if (currentImage == 4) {
+        $(this).hide();
+      }
     }
-
   });
   $("#left").click(function() {
+
     if (currentImage > 1) {
-    margin = margin + 1000;
-    $(".content").animate({"margin-left":margin+"px"});
-    currentImage--;
-}
+      $("#right").show();
+      margin = margin + 1000;
+      $(".content").animate({"margin-left":margin+"px"});
+      currentImage--;
+      if (currentImage ==1) {
+        $(this).hide();
+      }
+    }
   });
 });
